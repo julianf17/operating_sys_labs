6ec1bbaf29e24c327fdd21a24a4a8f977305326a
julianf17 <56906511+julianf17@users.noreply.github.com>
2022-12-16 14:43:57 -0500

    Project 2: Part 2

--- Project 2: Part 2/BENSCHILLIBOWL.c
+++ Project 2: Part 2/BENSCHILLIBOWL.c
@@ -38,6 +38,7 @@ BENSCHILLIBOWL* OpenRestaurant(int max_size, int expected_num_orders) {
   printf("Restaurant is open!\n");
   
   BENSCHILLIBOWL* bcb = (BENSCHILLIBOWL*) malloc(sizeof(BENSCHILLIBOWL));
+  
   bcb->orders = NULL;
   bcb->max_size = max_size;
   bcb->current_size = 0;
@@ -70,10 +71,13 @@ void recursive(Order* order) {
 
 void CloseRestaurant(BENSCHILLIBOWL* bcb) {
   printf("Restaurant is closed\n");
+  
   recursive(bcb->orders);
+  
   pthread_mutex_destroy(&bcb->mutex);
   pthread_cond_destroy(&bcb->can_add_orders);
   pthread_cond_destroy(&bcb->can_get_orders);
+  
   free(bcb);
 }
 
@@ -83,6 +87,7 @@ bool IsFull(BENSCHILLIBOWL* bcb) {
 
 void AddOrderToBack(Order **orders, Order *order) {
   Order *curr = *orders;
+  
   if (!curr) {
     *orders = order;
     return;
@@ -96,10 +101,14 @@ void AddOrderToBack(Order **orders, Order *order) {
 
 int AddOrder(BENSCHILLIBOWL* bcb, Order* order) {
   pthread_mutex_lock(&bcb->mutex);
+  
   int order_num = bcb->next_order_number;
+  
   if (!IsFull(bcb)) {
     order->order_number = bcb->next_order_number;
+    
     AddOrderToBack(&bcb->orders, order);
+    
     bcb->next_order_number++;
     bcb->current_size++;
   } else {
@@ -107,12 +116,15 @@ int AddOrder(BENSCHILLIBOWL* bcb, Order* order) {
       pthread_cond_wait(&bcb->can_add_orders, &bcb->mutex);
     }
     order->order_number = bcb->next_order_number;
+    
     AddOrderToBack(&bcb->orders, order);
+    
     bcb->next_order_number++;
     bcb->current_size++;
   }
   pthread_cond_signal(&bcb->can_get_orders);
   pthread_mutex_unlock(&bcb->mutex);
+  
   return order_num;
 }
 
@@ -123,6 +135,7 @@ bool IsEmpty(BENSCHILLIBOWL* bcb) {
 Order *GetOrder(BENSCHILLIBOWL* bcb) {
   pthread_mutex_lock(&bcb->mutex);
   Order* order;
+  
   struct timespec wait_time;
   struct timeval curr_time;
   
